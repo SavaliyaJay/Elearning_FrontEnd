83c46be9f162f9a8062332304ae76de44ee270f5
Jay Savaliya <jrsavaliya04@gmail.com>
2023-03-26 15:31:45 +0530

    about us page

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -4,49 +4,99 @@ const AboutUs = () => {
   return (
     <>
       <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
-          Basically 
-        </h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+            Basically
+          </h2>
+          <div className="mx-auto mt-10 grid max-w-lg grid-cols-3 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-5 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+            
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
-            alt="Transistor"
-            width={158}
-            height={48}
-          />
-          <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
-            alt="Reform"
-            width={158}
-            height={48}
-          />
-          <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
-            alt="Tuple"
-            width={158}
-            height={48}
-          />
-          <img
-            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
-            alt="SavvyCal"
-            width={158}
-            height={48}
-          />
-          <img
-            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
-            alt="Statamic"
-            width={158}
-            height={48}
-          />
+              className="col-span-2 col-start-2 max-h-10 w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="../assets/libAndFram/14.png"
+              alt="Statamic"
+              width={158}
+              height={48}
+            />
+            <img
+              className="col-span-2 col-start-2 max-h-10 w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="../assets/libAndFram/2.png"
+              alt="Statamic"
+              width={158}
+              height={48}
+            />
+            <img
+              className="col-span-2 col-start-2 max-h-12  w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="../assets/libAndFram/3.png"
+              alt="Statamic"
+              width={158}
+              height={48}
+            />
+            <img
+              className="col-span-2 col-start-2 max-h-10  w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="../assets/libAndFram/4.png"
+              alt="Statamic"
+              width={158}
+              height={48}
+            />
+            <img
+              className="col-span-2 col-start-2 max-h-10  w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="../assets/libAndFram/6.png"
+              alt="Statamic"
+              width={158}
+              height={48}
+            />
+            <img
+              className="col-span-2 col-start-2 max-h-10  w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="../assets/libAndFram/7.png"
+              alt="Statamic"
+              width={158}
+              height={48}
+            />
+            <img
+              className="col-span-2 col-start-2 max-h-10  w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="../assets/libAndFram/8.png"
+              alt="Statamic"
+              width={158}
+              height={48}
+            />
+            <img
+              className="col-span-2 col-start-2 max-h-10  w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="../assets/libAndFram/9.png"
+              alt="Statamic"
+              width={158}
+              height={48}
+            />
+            <img
+              className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="../assets/libAndFram/10.png"
+              alt="Statamic"
+              width={158}
+              height={48}
+            />
+            <img
+              className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="../assets/libAndFram/15.png"
+              alt="Statamic"
+              width={158}
+              height={48}
+            />
+            <img
+              className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="../assets/libAndFram/12.png"
+              alt="Statamic"
+              width={158}
+              height={48}
+            />
+            <img
+              className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="../assets/libAndFram/13.png"
+              alt="Statamic"
+              width={158}
+              height={48}
+            />
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
