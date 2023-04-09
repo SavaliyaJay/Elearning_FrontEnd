5b330b5e15c64df970f30454b6cb5d79f50434f9
Jay Savaliya <jrsavaliya04@gmail.com>
2023-04-09 09:53:23 +0530

    Description added

--- src/Pages/User/ContentOnBox.jsx
+++ src/Pages/User/ContentOnBox.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactPlayer from "react-player";
 
+function HigherText(props) {
+  return <div dangerouslySetInnerHTML={props.html} />;
+}
+
 const ContentOnBox = () => {
+  const url ="https://www.youtube.com/watch?v=ysz5S6PUM-U";
+  const htmlData = "<p>This is some <strong>formatted</strong> text.</p>";
+  const higherText = { __html: htmlData };
+
   return (
     <>
-      <div>
+      <div className="video">
         <h1>My Video Player</h1>
-        <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
+        <ReactPlayer
+          url={url}
+          controls={true}
+          width="100%"
+        />
+      </div>
+      <div className="description mt-10">
+        <h1>Description</h1>
+        <HigherText html={higherText} />
       </div>
     </>
   );
